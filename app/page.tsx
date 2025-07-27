ea784630c9a23274eb36204d903342258a69df69
lntcu <lntcu@duck.com>
2025-07-27 16:09:24 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="w-screen h-screen">
       <div className="w-full h-full flex items-end justify-start p-20">
         <div>
-          <p className="text-md font-bold">Daniel Goh</p>
+          <p className="text-md font-bold">Daniel</p>
           <p className="text-md text-zinc-500">Coming soon (likely 2026)</p>
         </div>
       </div>
