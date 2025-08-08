8693f84e327c913db8d42d6485f26d84cdbc6a7d
lntcu <lntcu@duck.com>
2025-08-08 19:00:33 +0800

    Update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,10 +4,10 @@ export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 my-5 mx-3 sm:mx-7 z-50">
       <div className="rounded-xl backdrop-blur-xs bg-zinc-800/50 flex items-center justify-center shadow-lg shadow-zinc-700/30 border-2 border-zinc-800/10 gap-10 sm:gap-20 px-3 py-1">
-        <Link href="/" className="font-medium active:scale-80">
+        <Link href="/" className="font-medium active:scale-80 max-sm:text-sm">
           Daniel
         </Link>
-        <div className="flex items-center justify-center gap-4">
+        <div className="flex items-center justify-center gap-4 max-sm:text-sm">
           <Link
             href="/"
             className="hover:text-white text-zinc-400 transition-all duration-300 active:scale-80"
