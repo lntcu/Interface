4668303e9be750f0b2788984f6d014506cb192de
lntcu <lntcu@duck.com>
2025-09-17 17:20:05 +0800

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Finder",
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased text-black bg-white`}>
         <div>{children}</div>
+        <Analytics />
       </body>
     </html>
   );
