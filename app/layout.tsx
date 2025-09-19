ff862c3e41ad008f62c2c4183d31f7c69dbe8525
lntcu <lntcu@duck.com>
2025-09-19 08:25:53 +0800

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased text-black bg-white `}>
+        <div className="fixed top-0 left-0 z-100 backdrop-blur-xl w-screen h-screen flex flex-col items-center justify-center">
+          <p className="font-medium text-lg">I might have deleted this.</p>
+          <p className="text-sm font-medium opacity-20">
+            Just delete this banner.
+          </p>
+        </div>
         <div>{children}</div>
         <Analytics />
         <SpeedInsights />
