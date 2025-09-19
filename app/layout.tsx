0341f934d01936100500d7bad20e03b29d1049d5
lntcu <lntcu@duck.com>
2025-09-19 09:38:55 +0800

    Ad

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "lHog",
@@ -16,13 +17,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  redirect(
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FEvrIzDBUYAM5W4u.jpg&f=1&nofb=1&ipt=275e80217f5e603f1be1edf6025656d6e5c0cd57980870eccf5b07309139d4ff",
+  );
+
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased text-black bg-white `}>
-        <div className="fixed top-0 left-0 z-100 backdrop-blur-xl w-screen h-screen flex flex-col items-center justify-center pointer-events-none">
-          <p className="font-medium text-lg">Seems a bit blur.</p>
-        </div>
-        <div>{children}</div>
+      <body className={`${inter.className} antialiased text-black bg-white`}>
+        {children}
         <Analytics />
         <SpeedInsights />
       </body>
