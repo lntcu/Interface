d3b98d701f94ed39872e6f709135dd22644b31af
lntcu <lntcu@duck.com>
2025-09-19 20:56:41 +0800

    Removed ad

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  redirect(
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FEvrIzDBUYAM5W4u.jpg&f=1&nofb=1&ipt=275e80217f5e603f1be1edf6025656d6e5c0cd57980870eccf5b07309139d4ff",
-  );
-
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased text-black bg-white`}>
