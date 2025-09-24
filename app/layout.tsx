fc97dea12303333cd806f4f55a1c459c86a9b8d2
lntcu <lntcu@duck.com>
2025-09-24 11:05:27 +0800

    Remove

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,15 +24,6 @@ export default function RootLayout({
         className={`${inter.className} antialiased leading-5 text-black bg-white flex items-center justify-center flex-col w-full h-full`}
       >
         <div className="max-w-lg py-30">{children}</div>
-        <div className="fixed top-0 left-0 z-1000 p-20 flex pointer-events-none items-center justify-center w-screen h-screen">
-          <Image
-            src="/ad.jpeg"
-            width={5000}
-            height={5000}
-            alt="Advert"
-            className="h-screen w-auto object-fit p-10"
-          />
-        </div>
         <Analytics />
         <SpeedInsights />
       </body>
